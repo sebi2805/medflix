36d3ef1c393d30b12a99863a2c0166d730476bef
Virtopeanu Sebastian <f_seby@yahoo.com>
2023-08-21 20:35:22 +0300

    aded changes

--- components/homepage/index.tsx
+++ components/homepage/index.tsx
@@ -4,7 +4,7 @@ import PresentationSection from "./PresentationSection";
 import ServicesSection from "./ServicesSection";
 
 export default function HomePage() {
-  const targetDate = new Date("July 19, 2023 00:00:00");
+  const targetDate = new Date("July 19, 2024 00:00:00");
 
   return (
     <div className="w-full bg-whiteBackground flex justify-center items-center flex-col">
