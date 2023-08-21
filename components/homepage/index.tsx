c6367ddfd11916aac0de3aa8a57ae9276e809b02
Virtopeanu Sebastian <f_seby@yahoo.com>
2023-08-21 20:20:24 +0300

    added changes

--- components/homepage/index.tsx
+++ components/homepage/index.tsx
@@ -4,7 +4,7 @@ import PresentationSection from "./PresentationSection";
 import ServicesSection from "./ServicesSection";
 
 export default function HomePage() {
-  const targetDate = new Date("December 25, 2023 00:00:00"); // Placeholder for Christmas
+  const targetDate = new Date("July 19, 2023 00:00:00");
 
   return (
     <div className="w-full bg-whiteBackground flex justify-center items-center flex-col">
